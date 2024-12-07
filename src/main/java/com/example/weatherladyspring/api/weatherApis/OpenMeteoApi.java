3196429601b7f6ac56ae8e1945c8816f706b9ba1
christosste86 <christosste86@gmail.com>
2024-12-07 07:58:48 +0100

    add weather properties

--- src/main/java/com/example/weatherladyspring/api/weatherApis/OpenMeteoApi.java
+++ src/main/java/com/example/weatherladyspring/api/weatherApis/OpenMeteoApi.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class OpenMeteoApi {
-    //https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m
+    //https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&hourly=temperature_2m,precipitation,rain,showers,snowfall,snow_depth
     private final ApiConnect openMeteo;
     private final Double latitude;
     private final Double longitude;
@@ -20,7 +20,7 @@ public class OpenMeteoApi {
     public OpenMeteoApi(Double latitude, Double longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
-        String url = String.format("https://api.open-meteo.com/v1/forecast?latitude=%s2&longitude=%s&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m", latitude, longitude);
+        String url = String.format("https://api.open-meteo.com/v1/forecast?latitude=%s2&longitude=%s&hourly=temperature_2m,rain,snowfall,cloud_cover,wind_speed_10m,relative_humidity_2m,apparent_temperature,is_day,precipitation,pressure_msl,surface_pressure,wind_direction_10m,wind_gusts_10m", latitude, longitude);
         this.openMeteo = new ApiConnect(url);
     }
 
@@ -45,19 +45,73 @@ public class OpenMeteoApi {
         return hourlyObject().getJSONArray("relative_humidity_2m");
     }
 
-    private JSONArray windArray(){
+    private JSONArray windSpeedArray(){
         return hourlyObject().getJSONArray("wind_speed_10m");
     }
 
+    private JSONArray rainArray(){
+        return hourlyObject().getJSONArray("rain");
+    }
+
+    private JSONArray cloudCoverArray(){
+        return hourlyObject().getJSONArray("cloud_cover");
+    }
+
+    private JSONArray snowfallArray(){
+        return hourlyObject().getJSONArray("snowfall");
+    }
+
+    private JSONArray relativeHumidityArray(){
+        return hourlyObject().getJSONArray("relative_humidity_2m");
+    }
+
+    private JSONArray apparentTemperatureArray(){
+        return hourlyObject().getJSONArray("apparent_temperature");
+    }
+
+    private JSONArray isDayArray(){
+        return hourlyObject().getJSONArray("is_day");
+    }
+
+    private JSONArray precipitationArray(){
+        return hourlyObject().getJSONArray("precipitation");
+    }
+
+    private JSONArray pressureMslArray(){
+        return hourlyObject().getJSONArray("pressure_msl");
+    }
+
+    private JSONArray surfacePressureArray(){
+        return hourlyObject().getJSONArray("surface_pressure");
+    }
+
+    private JSONArray windDirectionArray(){
+        return hourlyObject().getJSONArray("wind_direction_10m");
+    }
+
+    private JSONArray windGustsArray(){
+        return hourlyObject().getJSONArray("wind_gusts_10m");
+    }
 
     public List<Weather> getWeatherLocationPerHour(){
         List<Weather> weatherLocationPerHour = new ArrayList<>();
         for (int i = 0; i < timeArray().length(); i++) {
             LocalDateTime time = LocalDateTime.parse(timeArray().getString(i));
+            Boolean isDay = isDayArray().getInt(i) != 0;
             Double temperatureInC = temperatureArray().getDouble(i);
+            Double apparentTemperature = apparentTemperatureArray().getDouble(i);
             Integer humidity = humidityArray().getInt(i);
-            Double windSpeed = windArray().getDouble(i);
-            weatherLocationPerHour.add(new Weather(time, this.latitude, this.longitude, temperatureInC, humidity, windSpeed));
+            Integer relativeHumidity= relativeHumidityArray().getInt(i);
+            Double windSpeed = windSpeedArray().getDouble(i);
+            Integer windDirection = windDirectionArray().getInt(i);
+            Integer windGusts = windGustsArray().getInt(i);
+            Integer cloudCover = cloudCoverArray().getInt(i);
+            Double rain = rainArray().getDouble(i);
+            Double snowfall = snowfallArray().getDouble(i);
+            Double precipitation = precipitationArray().getDouble(i);
+            Double pressureMsl = pressureMslArray().getDouble(i);
+            Double surfacePressure = surfacePressureArray().getDouble(i);
+            weatherLocationPerHour.add(new Weather(time, isDay, latitude, longitude, temperatureInC, apparentTemperature, humidity, relativeHumidity, windSpeed, windDirection, windGusts, cloudCover, rain, snowfall, precipitation, pressureMsl, surfacePressure));
         }return weatherLocationPerHour;
     }
 }
--- src/main/java/com/example/weatherladyspring/models/Weather.java
+++ src/main/java/com/example/weatherladyspring/models/Weather.java
@@ -1,31 +1,89 @@
 package com.example.weatherladyspring.models;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 
-
+@Entity
+@Table(name = "weathers")
 public class Weather {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @ManyToOne
-    private Location location;
-    private Long locationId;
     private final LocalDateTime time;
+    private final Boolean isDay;
     private final Double latitude;
     private final Double longitude;
+    @Column (name = "temperature_c")
     private final Double temperatureInC;
-    private final Integer humidity ;
+    @Column (name = "apparent_temperature")
+    private final Double apparentTemperature;
+    private final Integer humidity;
+    @Column (name ="relative_humanity")
+    private final Integer relativeHumidity;
+    @Column (name ="wind_speed")
     private final Double windSpeed;
+    @Column (name = "wind_direction")
+    private final Integer windDirection;
+    @Column (name = "wind_gusts")
+    private final Integer windGusts;
+    @Column (name ="cloud_cover")
+    private final Integer cloudCover;
+    private final Double rain;
+    private final Double snowfall;
+    private final Double precipitation;
+    private final Double pressureMsl;
+    @Column (name = "surface_pressure")
+    private final Double surfacePressure;
 
-    public Weather(LocalDateTime time, Double latitude, Double longitude, Double temperatureInC, Integer humidity, Double windSpeed) {
+//    @ManyToOne
+//    @JoinColumn(name = "location_id")
+//    private Location location;
+    private Long locationId;
+
+
+    public Weather(LocalDateTime time, Boolean isDay, Double latitude, Double longitude, Double temperatureInC, Double apparentTemperature, Integer humidity, Integer relativeHumidity, Double windSpeed, Integer windDirection, Integer windGusts, Integer cloudCover, Double rain, Double snowfall, Double precipitation, Double pressureMsl, Double surfacePressure) {
+
         this.time = time;
+        this.isDay = isDay;
         this.latitude = latitude;
         this.longitude = longitude;
         this.temperatureInC = temperatureInC;
+        this.apparentTemperature = apparentTemperature;
         this.humidity = humidity;
+        this.relativeHumidity = relativeHumidity;
         this.windSpeed = windSpeed;
+        this.windDirection = windDirection;
+        this.windGusts = windGusts;
+        this.cloudCover = cloudCover;
+        this.rain = rain;
+        this.snowfall = snowfall;
+        this.precipitation = precipitation;
+        this.pressureMsl = pressureMsl;
+        this.surfacePressure = surfacePressure;
     }
+
+    public Weather() {
+        this.time = null;
+        this.isDay = null;
+        this.latitude = null;
+        this.longitude = null;
+        this.temperatureInC = null;
+        this.apparentTemperature = null;
+        this.humidity = null;
+        this.relativeHumidity = null;
+        this.windSpeed = null;
+        this.windDirection = null;
+        this.windGusts = null;
+        this.cloudCover = null;
+        this.rain = null;
+        this.snowfall = null;
+        this.precipitation = null;
+        this.pressureMsl = null;
+        this.surfacePressure = null;
+    }
+
 
     public Long getId() {
         return id;
@@ -35,16 +93,62 @@ public class Weather {
         this.id = id;
     }
 
-    public void setLocationId(Long locationId) {
-        this.locationId = locationId;
+    public Boolean getDay() {
+        return isDay;
+    }
+
+    public Double getApparentTemperature() {
+        return apparentTemperature;
+    }
+
+//    public void setLocation(Location location) {
+//        this.location = location;
+//    }
+
+    public Integer getRelativeHumidity() {
+        return relativeHumidity;
+    }
+
+    public Integer getWindDirection() {
+        return windDirection;
+    }
+
+    public Integer getWindGusts() {
+        return windGusts;
+    }
+
+    public Double getPrecipitation() {
+        return precipitation;
+    }
+
+    public Double getPressureMsl() {
+        return pressureMsl;
+    }
+
+    public Double getSurfacePressure() {
+        return surfacePressure;
+    }
+
+
+
+    public Integer getCloudCover() {
+        return cloudCover;
+    }
+
+    public Double getRain() {
+        return rain;
+    }
+
+    public Double getSnowfall() {
+        return snowfall;
     }
 
     public Long getLocationId() {
         return locationId;
     }
 
-    public Location getLocation() {
-        return location;
+    public void setLocationId(Long locationId) {
+        this.locationId = locationId;
     }
 
     public LocalDateTime getTime() {
@@ -73,6 +177,25 @@ public class Weather {
 
     @Override
     public String toString() {
-        return String.format("%slocationId: %s [%s - %s] Temp: %s°C, Hum: %s%%, Wind: %skm/h", this.locationId, this.time, this.latitude, this.longitude, this.temperatureInC, this.humidity, this.windSpeed);
+        return "Weather{" +
+                "id=" + id +
+                ", time=" + time +
+                ", isDay=" + isDay +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", temperatureInC=" + temperatureInC +
+                ", apparentTemperature=" + apparentTemperature +
+                ", humidity=" + humidity +
+                ", relativeHumidity=" + relativeHumidity +
+                ", windSpeed=" + windSpeed +
+                ", windDirection=" + windDirection +
+                ", windGusts=" + windGusts +
+                ", cloudCover=" + cloudCover +
+                ", rain=" + rain +
+                ", snowfall=" + snowfall +
+                ", precipitation=" + precipitation +
+                ", pressureMsl=" + pressureMsl +
+                ", surfacePressure=" + surfacePressure +
+                '}';
     }
 }
